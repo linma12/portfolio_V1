3ce63b2fc95da71ad8d1c95862e0011eef1871d6
Matthieu <51370572+linma12@users.noreply.github.com>
2021-06-01 18:16:41 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,87 @@
+const animatedText = document.getElementById("animated-text");
+var newJob = "Front-end developper.";
+const h2 = document.querySelector("h2");
+var jobDescription = h2.innerHTML;
+var j = 0;
+var newJobDescr= "";
+
+console.log(h2.innerHTML.substring(0, h2.innerHTML.length - 1))
+
+function setTick() {
+    if (animatedText.style.borderRightStyle == "solid"){
+        animatedText.style.borderRightStyle = "";
+    }else{
+        animatedText.style.borderRightStyle = "solid";
+    }
+}
+
+var myVar = setInterval(setTick, 500);
+
+function typeWriter(){
+    if(newJob.length != h2.innerHTML.length){
+        h2.innerHTML = h2.innerHTML.concat(newJob.charAt(j))
+        j++;
+        setTimeout(typeWriter, 200)
+        return
+    }
+    clearInterval(myVar);
+    animatedText.style.borderRightStyle = "";
+}
+function deleteText(){
+    if(h2.innerHTML.length){
+        h2.innerHTML = h2.innerHTML.substring(0, h2.innerHTML.length - 1)
+        setTimeout(deleteText, 100);
+        return;
+    }
+    typeWriter()
+}
+
+deleteText()
+
+
+const travail = document.querySelector(".jobs");
+
+class experience{
+    constructor(company, jobDescr, place, startDate, endDate, url){
+        this.company = company;
+        this.jobDescr = jobDescr;
+        this.place = place;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.url = url;
+    }
+};
+
+const ekium = new experience("Ekium", "Automation Project Engineer", "Lyon", "Setpember 2015", "January 2021", "https://www.ekium.eu/")
+const genzyme = new experience("Genzyme", "Automation Engineer", "Lyon", "January 2014", "July 2015", "https://www.sanofi.com/en/your-health/specialty-care")
+
+const Jobs = [genzyme, ekium];
+var position = 0;
+
+Jobs.forEach(element => {
+    const section = document.createElement("section");
+    section.setAttribute("class", "work-experience")
+    section.setAttribute("data-id", element.company)
+    section.setAttribute("data-state", "small")
+    const content = `<h3>${element.jobDescr}<br><a href="${element.url}">@ ${element.company}</a><h3>`;
+    section.style.position = "relative";
+    section.style.left = position.toString().concat("px");
+    position += 100;
+    section.innerHTML = content;
+    travail.append(section);
+})
+
+const allMyJobs = document.querySelectorAll(".work-experience")
+
+allMyJobs.forEach(element => {
+    element.addEventListener("click", () =>{
+        element.setAttribute("data-state","big")
+        element.style.position = "static";
+        element.style.left = "0px";
+        console.log("i clicked on", element.getAttribute("data-id"));
+    })
+})
+
+
+
+
